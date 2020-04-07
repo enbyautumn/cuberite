d4d712ddc75e1c48711c845cbb7c61aa822bbac8
enbyautumn <60362123+enbyautumn@users.noreply.github.com>
2020-04-07 18:53:56 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -17,7 +17,7 @@ DEFAULT_THREADS=1
 
 # Constants not modifiable through command line:
 UPSTREAM_REPO="origin"
-UPSTREAM_LINK="https://github.com/cuberite/cuberite.git"
+UPSTREAM_LINK="https://github.com/enbyautumn/cuberite.git"
 
 #=================== Error functions ===================
 
